ef05f8d4b573f4a4ddaf7488b4abd4d1169b2215
bakerycookies <pingchenhsu@gmail.com>
2022-07-12 21:59:48 +0800

    django add dataTable.js

--- pi/staff/views.py
+++ pi/staff/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from staff.models import *
 
 # Function to render Homepage
 def home(request):
-    return render(request, "home.html")
+    employee_list = Employee.objects.all()
+    return render(request, "home.html", {"employees":employee_list})
